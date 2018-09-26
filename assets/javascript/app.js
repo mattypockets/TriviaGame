8fe41ad76ef92902c0d8076081895be8190c9924
Matt Gasper <mrgasper27@gmail.com>
2018-09-26 09:52:10 -0500

    Added trivia questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+const question1 = {
+    "question" : "Hal Emmerich goes by which code name?",
+    "false1" : "Drebin",
+    "false2" : "Revolver Ocelot",
+    "false3" : "Grey Fox",
+    "answer" : "Otacon",
+};
+
+const question2 = {
+    "question" : "The Metal Gear at the center of the Shadow Moses incident was named:",
+    "false1" : "Ray",
+    "answer" : "Rex",
+    "false2" : "Zeke",
+    "false3" : "Fox",
+};
+
+const question3 = {
+    "question" : "What was the name of Big Boss' independent military organization?",
+    "false1" : "Foxhound",
+    "false2" : "Shagohod",
+    "answer" : "Outer Heaven",
+    "false3" : "The Patriots",
+};
+
+const question4 = {
+    "question" : "The President of the United States during the early 2000s was also known by this codename:",
+    "answer": "Solidus Snake",
+    "false1": "Solid Snake",
+    "false2": "Liquid Snake",
+    "false3": "Punished Snake",
+};
+
+const question5 = {
+    "question" : "The project that created the clones of Big Boss was also known as:",
+    "false1": "Militaires Sans Frontieres",
+    "answer": "Les Enfants Terrible",
+    "false2": "Outer Heaven",
+    "false3": "Diamond Dogs",
+};
+
+const questions = {question1, question2, question3, question4, question5};
